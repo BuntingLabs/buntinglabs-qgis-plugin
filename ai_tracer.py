14f8878120a1e3a68405736c7782b4927576f0c7
Brendan Ashworth <brendan.ashworth@me.com>
2024-01-01 16:27:19 -0800

    manually manage rubber band

--- ai_tracer.py
+++ ai_tracer.py
@@ -1,10 +1,12 @@
 # Copyright 2023 Bunting Labs, Inc.
 
-from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtCore import Qt, QSettings
 from qgis.PyQt.QtWidgets import QPushButton
-from qgis.gui import QgsMapToolCapture
+from qgis.PyQt.QtGui import QColor
+from qgis.gui import QgsMapToolCapture, QgsRubberBand
 from qgis.core import Qgis, QgsFeature, QgsApplication, QgsPointXY, \
-    QgsGeometry, QgsPolygon, QgsProject, QgsVectorLayer, QgsRasterLayer
+    QgsGeometry, QgsPolygon, QgsProject, QgsVectorLayer, QgsRasterLayer, \
+    QgsPoint, QgsWkbTypes
 
 from .tracing_task import AutocompleteTask
 
@@ -20,7 +22,7 @@ class AIVectorizerTool(QgsMapToolCapture):
         super(AIVectorizerTool, self).__init__(plugin.iface.mapCanvas(), cadDockWidget, current_mode)
 
         self.plugin = plugin
-        self.current_mode = current_mode
+        self.rb = self.initRubberBand(current_mode)
 
         # Options
         self.num_completions = 50
@@ -36,6 +38,26 @@ class AIVectorizerTool(QgsMapToolCapture):
         # will follow the moving cursor, once there's a vertex in the chamber
         self.startCapturing()
 
+    def initRubberBand(self, mode):
+        if mode == QgsMapToolCapture.CaptureLine:
+            rb = QgsRubberBand(self.plugin.iface.mapCanvas(), QgsWkbTypes.LineGeometry)
+        elif mode == QgsMapToolCapture.CapturePolygon:
+            rb = QgsRubberBand(self.plugin.iface.mapCanvas(), QgsWkbTypes.PolygonGeometry)
+        else:
+            raise ValueError
+
+        rb.setFillColor(self.digitizingFillColor())
+        rb.setColor(self.digitizingStrokeColor())
+        rb.setWidth(self.digitizingStrokeWidth())
+        rb.setLineStyle(Qt.DotLine)
+
+        color = self.digitizingStrokeColor()
+
+        alpha_scale = QSettings().value("Qgis/digitizing/line_color_alpha_scale", 1.0, type=float)
+        color.setAlphaF(color.alphaF() * alpha_scale)
+
+        return rb
+
     def notifyUserOfAuthError(self, e):
         widget = self.plugin.iface.messageBar().createMessage("Error", "Could not call AI vectorizer: %s." % e)
         button = QPushButton(widget)
@@ -43,6 +65,71 @@ class AIVectorizerTool(QgsMapToolCapture):
         button.pressed.connect(self.plugin.openSettings)
         widget.layout().addWidget(button)
         self.plugin.iface.messageBar().pushWidget(widget, Qgis.Critical, duration=15)
+
+    def handlePointReceived(self, args):
+        self.addVertex(QgsPointXY(*args[0]))
+        self.vertices.append(QgsPointXY(*args[0]))
+
+    def shiftClickAdjustment(self, pos, trimToPoint=False):
+        pt = self.toMapCoordinates(pos)
+
+        # Return the geometry prior
+        last_point_idx = sorted(enumerate(self.vertices), key=lambda v: (v[1].x()-pt.x())**2 + (v[1].y()-pt.y())**2)[0][0]
+
+        # Find the closest point
+        last_point = QgsPointXY(self.vertices[last_point_idx].x(), self.vertices[last_point_idx].y())
+
+        # Geometry for polygon
+        points = [ QgsPointXY(v.x(), v.y()) for v in self.vertices[:last_point_idx] ]
+        # Close it!
+        points += [ QgsPointXY(last_point.x(), last_point.y()), QgsPointXY(pt.x(), pt.y()), points[0]]
+
+        poly_geo = QgsGeometry.fromPolygonXY([points])
+
+        if trimToPoint:
+            numToTrim = len(self.vertices)-last_point_idx
+            for _ in range(numToTrim):
+                self.undo()
+            self.vertices = self.vertices[:-numToTrim]
+
+        return (last_point, poly_geo)
+
+    def canvasMoveEvent(self, e):
+        if len(self.vertices) == 0:
+            # Nothing to do!
+            return
+
+        pt = self.toMapCoordinates(e.pos())
+
+        # Check if the shift key is being pressed
+        # We hide the geometry rubber band when the shift button goes down
+        if e.modifiers() & Qt.ShiftModifier:
+            # Shift means our last vertex should effectively be the closest point to the line
+            (last_point, poly_geo) = self.shiftClickAdjustment(e.pos())
+        else:
+            last_point = QgsPointXY(self.vertices[-1].x(), self.vertices[-1].y())
+
+            # Close it!
+            points = self.vertices + [ QgsPointXY(pt.x(), pt.y()), self.vertices[0]]
+
+            poly_geo = QgsGeometry.fromPolygonXY([points])
+
+        # geometry depends on capture mode
+        if self.mode() == QgsMapToolCapture.CaptureLine or (len(self.vertices) <= 2):
+            points = [last_point, QgsPointXY(pt.x(), pt.y())]
+            self.rb.setToGeometry(
+                QgsGeometry.fromPolylineXY(points),
+                None
+            )
+        elif self.mode() == QgsMapToolCapture.CapturePolygon:
+            # points = [ QgsPointXY(v.x(), v.y()) for v in self.vertices[:last_point_idx] ]
+            # Close it!
+            # points += [ QgsPointXY(last_point.x(), last_point.y()), QgsPointXY(pt.x(), pt.y()), points[0]]
+
+            self.rb.setToGeometry(
+                poly_geo,
+                None
+            )
 
     def canvasPressEvent(self, e):
         pass
@@ -75,10 +162,10 @@ class AIVectorizerTool(QgsMapToolCapture):
             # Null fields
             f = QgsFeature(vlayer.fields(), 0)
 
-            if self.current_mode == QgsMapToolCapture.CaptureLine:
+            if self.mode() == QgsMapToolCapture.CaptureLine:
                 g = QgsGeometry(curve.curveToLine())
                 f.setGeometry(g)
-            elif self.current_mode == QgsMapToolCapture.CapturePolygon:
+            elif self.mode() == QgsMapToolCapture.CapturePolygon:
                 poly = QgsPolygon()
                 poly.setExteriorRing(curve.curveToLine())
 
@@ -91,7 +178,11 @@ class AIVectorizerTool(QgsMapToolCapture):
             self.vertices = []
 
             self.stopCapturing()
+            self.rb.reset()
         elif e.button() == Qt.LeftButton:
+            if e.modifiers() & Qt.ShiftModifier:
+                self.shiftClickAdjustment(e.pos(), trimToPoint=True)
+
             # QgsPointXY with map CRS
             point = self.toMapCoordinates(e.pos())
 
@@ -103,7 +194,7 @@ class AIVectorizerTool(QgsMapToolCapture):
             self.startCapturing()
 
             # Create our autocomplete task if we have >=2 vertices
-            if len(self.vertices) >= 2:
+            if len(self.vertices) >= 2 and not (e.modifiers() & Qt.ShiftModifier):
                 root = QgsProject.instance().layerTreeRoot()
                 rlayers = [node.layer() for node in root.children() if isinstance(node.layer(), QgsRasterLayer)]
                 vlayer = self.plugin.iface.activeLayer()
@@ -117,9 +208,12 @@ class AIVectorizerTool(QgsMapToolCapture):
                     project_crs
                 )
 
-                self.autocomplete_task.pointReceived.connect(lambda args: self.addVertex(QgsPointXY(*args[0])))
+                self.autocomplete_task.pointReceived.connect(lambda args: self.handlePointReceived(args))
                 self.autocomplete_task.errorReceived.connect(lambda e: self.notifyUserOfAuthError(e))
 
                 QgsApplication.taskManager().addTask(
                     self.autocomplete_task,
                 )
+
+    def deactivate(self):
+        self.rb.reset()
